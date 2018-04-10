840d2cbaec893a231530e87ce189f568131ee011
AndrewOlivier <38205514+AndrewOlivier@users.noreply.github.com>
2018-04-10 13:40:41 +0200

    a

--- decodeDTMF.m
+++ decodeDTMF.m
@@ -1,7 +1,8 @@
- filename = 'abcd.wav';
+ filename = 'dtmfM6.wav';
 % Function to decode an audio file with DTMF Tones and return the sequence
     if (isstr(filename))
         [data,Fs] = audioread(filename);
+        soundsc(data,Fs)
     else 
        data = filename;
        Fs = 8000;
