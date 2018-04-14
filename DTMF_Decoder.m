1c43502b50a27de2353485c054a66c1ba0dcbd50
AndrewOlivier <38205514+AndrewOlivier@users.noreply.github.com>
2018-04-14 11:59:55 +0200

    l

--- DTMF_Decoder.m
+++ DTMF_Decoder.m
@@ -1,7 +1,9 @@
 %Decode audio file with DTMF Tones and return values
 
-    [data,Fs] = audioread('abcd.wav');
-
+    [data,Fs] = audioread('dtmfN3.wav');
+    
+    plot(data); xlabel('Samples'); ylabel('Amplitude');   
+       
     blocks = createBlocks(data);
     data = DetectTones(blocks,Fs);
     Values = getValues(data)
--- createBlocks.m
+++ createBlocks.m
@@ -2,8 +2,8 @@
 function blocks = createBlocks (data)
 	%Make blocks of size "block size" and return
     %a matrix with each of the columns as a seperate block for processing
-
-    blockSize = 370;
+    Fs=8000;
+    blockSize = 400;
 
     if (length(data) < blockSize)
         blockSize = length(data);
@@ -16,14 +16,19 @@ function blocks = createBlocks (data)
     blocks = zeros(blockSize,nBlocks); 
     
     col = 1;
+    
     for i= 1 : floor(blockSize/2) : length(data)
         new =  data(i:i+blockSize-1);
-        blocks(:,col) = new;
+        blocks(:,col) = new;      
         if (col == nBlocks) % break when all the blocks have been created
             break;
         end
         col = col+1;
         
     end 
+    
+        figure
+        a_plot = new(:,1);
+        plot(a_plot); xlabel('Samples'); ylabel('Amplitude');
     
 end 
